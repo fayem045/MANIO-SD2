5c0c669a6992b9c32112457dbff2e0c5d13bc1e4
fayem045 <maniojoannafaye@gmail.com>
2026-03-12 11:56:01 +0800

    session14

---
+++ session14/index.js
@@ -0,0 +1,88 @@
+// This is how we print output using JS
+//console.log(stringContent|variable)
+console.log("Hello World");
+
+// To use comment we need to use ->
+// This is a single line comment//
+
+/* This is a 
+multi line
+ comment */
+
+//[SECTION] VARIABLES
+// Acts as a storage for a data which can be modify afterwords except constant variables
+
+//[SECTION] data types
+
+//NUMERIC DATA TYPES
+
+/*
+
+let VariableName; -> no initialized value
+let VariableName = value; -> initialized value
+
+*/
+let age; //initial var
+let height = 160;
+
+//shortcut: clg tab
+console.log(height);
+
+//JS is a loose-type PL
+console.log(height)
+
+console. log ( height   )
+
+console
+
+.
+
+
+log
+
+(height)
+
+// SYNCRONOUS PL -> JS
+
+let gwa = 99.75;
+console.log(gwa);
+
+//Character data types
+let firstName = "John";
+
+console.log(firstName);
+
+//TYPEOF -> to check the data type of a variable's type of data
+//all number sa java script, and know the data type automatically
+console.log(typeof firstName);
+console.log(typeof gwa);
+console.log(typeof height);
+
+//TEXT CASING IN JS
+//camel Casing -> thisIsCamelCasing
+//Snake Casing -> this_is_snake_casing
+//Pascal Casing -> ThisIsPascalCasing. UOPOER CASE PER WORD
+//kebab Casing -> this-is-kebab-casing
+//Pascal Kebab Casing -> This-Is-Pascal-Kebab-Casing
+
+//BOOLEAN DATA TYPES: make sure answerable to yes or no(best practice)
+let isMale = true;
+let isFemale = false;
+let hasExpired = true;
+
+console.log(isMarried);
+console.log(isRegistered);
+
+console.log(typeof isMarried);
+
+//COINCATENATION
+let province = "Pampanga";
+
+console.log("I Live in " province);
+//Type-Coercion
+//Concatenation of strinbg and a number ir othe rdata types will be converter to string. js auto conbvert in different types
+console.log("My overall gwa is " + gwa);
+
+// iNT
+// float:15 decimals
+// double:15 decimals, mas accurate
